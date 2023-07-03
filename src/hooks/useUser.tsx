2aa9e5141f46216eaa25917ac67d760777f4ee47
JisongImSorry <116711095+JisongImSorry@users.noreply.github.com>
2023-07-03 23:02:20 +0900

    bugfix

--- src/hooks/useUser.tsx
+++ src/hooks/useUser.tsx
@@ -2,7 +2,7 @@ import { useContext, useEffect, useState } from "react";
 
 export const useUser = () => {
   const [chain, setChain] = useState<string>("Ethereum");
-  const [id, setId] = useState<number>(0);
+  const [id, setId] = useState<number>(1);
   return {
     chain,
     id,
